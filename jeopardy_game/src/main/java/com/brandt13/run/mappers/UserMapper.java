e6bb39e201e2f24dff9ff521ce5366546b5312e7
brandt13 <brandtleeds@gmail.com>
2018-10-17 17:46:14 -0500

    Jeopardy 0.4.1 update

--- jeopardy_game/src/main/java/com/brandt13/run/mappers/UserMapper.java
+++ jeopardy_game/src/main/java/com/brandt13/run/mappers/UserMapper.java
@@ -6,10 +6,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
 
-        String CREATE_USER = "INSERT INTO `jeopardyGame`.`user` (`email`,`password`,`current_score`, `high_score`) " +
-                "VALUES (#{email}, #{password}, #{current_score}, #{high_score});";
-
-        String UPDATE_HIGH_SCORE =
+//        String CREATE_USER = "INSERT INTO `jeopardyGame`.`user` (`email`,`password`,`current_score`, `high_score`) " +
+//                "VALUES (#{email}, #{password}, #{current_score}, #{high_score});";
+//
+//        String UPDATE_HIGH_SCORE = '';
 
 //        @Create(CREATE_USER)
 //        User createUser(User user);
